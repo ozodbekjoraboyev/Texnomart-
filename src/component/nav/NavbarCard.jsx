73ab0d96e3419e60106881c5e5245e301360f982
unknown <baxtiyor.gazimboyev@gmail.com>
2025-02-13 15:59:31 +0300

    wefrgth

--- src/component/nav/NavbarCard.jsx
+++ src/component/nav/NavbarCard.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useMyStore from "../../ma-zustand";
 import { useParams } from "react-router-dom";
